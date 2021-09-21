0118c84183f906b660916cf482753384098b1463
Ligia D'Avila Bozzi <ligia@respondeai.com.br>
2021-09-21 09:22:06 -0300

    preparando review

--- src/styles/PageContentStyle.js
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components";
-
-export const PageContent = styled.div`
-  background-color: #E5E5E5;
-  height: 100vh;
-  padding: 1.313rem 1.125rem;
-  padding-top: 6.25rem;
-`
